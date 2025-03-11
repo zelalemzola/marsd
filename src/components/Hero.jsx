6661401e365749b7b5528aa570c64b2764e267c1
zelalemzola <tzelalemtesfaye@gmail.com>
2025-03-11 15:00:08 +0300

    done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,8 +10,8 @@ const Hero = () => {
       <h1 className='text-2xl md:text-9xl text-primary font-bold'>Mars Design</h1>
       <p className='text-lg md:text-2xl'>"Timeless Elegance, Ethiopian Heritage, Unparalleled Craftsmanship."</p>
       <div className="absolute -z-20 top-40 w-full hidden md:flex items-center justify-between">
-        <Image src='/hero1.png' alt='hero1' width={420} height={220}/>
-        <Image src='/hero2.png' alt='hero1' width={420} height={220} className=' -mt-[20%]'/>
+        <Image src='/hero1.png' alt='hero1' width={400} height={400}/>
+        <Image src='/hero2.png' alt='hero1' width={400} height={400} className=' -mt-[20%]'/>
       </div>
       <div className='flex flex-wrap items-center gap-6'>
         <Link className='py-2 px-4 rounded-full border-1 border-primary flex items-center gap-3' href='/'>Explore Collections <ArrowRightIcon/></Link>
